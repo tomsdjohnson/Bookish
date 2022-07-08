44ec3fe0f7a7303bc4b25b69a5ca671dfbb0ce55
Jordi Frittoli <Jordi.Frittoli@softwire.com>
2022-07-08 15:34:20 +0100

    Implemented Parts 2 and 1

--- bookish/controllers/bookish.py
+++ bookish/controllers/bookish.py
@@ -1,5 +1,5 @@
 from flask import request
-from bookish.models.example import Example
+from bookish.models.example import BookModel, BookCopies, BorrowedBooks, Users
 from bookish.models import db
 
 
@@ -8,24 +8,35 @@ def bookish_routes(app):
     def health_check():
         return {"status": "OK"}
 
-    @app.route('/example', methods=['POST', 'GET'])
-    def handle_example():
-        if request.method == 'POST':
-            if request.is_json:
-                data = request.get_json()
-                new_example = Example(data1=data['data1'], data2=data['data2'])
-                db.session.add(new_example)
-                db.session.commit()
-                return {"message": "New example has been created successfully."}
-            else:
-                return {"error": "The request payload is not in JSON format"}
+    @app.route('/AddBook', methods=['POST'])
+    def handle_AddBook():
+        if request.is_json:
 
-        elif request.method == 'GET':
-            examples = Example.query.all()
-            results = [
-                {
-                    'id': example.id,
-                    'data1': example.data1,
-                    'data2': example.data2
-                } for example in examples]
-            return {"examples": results}
+            data = request.get_json()
+            new_book_model = BookModel(ISBN=data['ISBN'], title=data['title'], author=data['author'])
+
+            print(BookCopies.query.all())
+
+            for x in range(data['copies']):
+                new_book_copy = BookCopies(ISBN=data['ISBN'])
+                db.session.add(new_book_copy)
+
+            print(BookCopies.query.all())
+
+            db.session.add(new_book_model)
+
+            db.session.commit()
+            return {"message": "New example has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
+
+    @app.route('/GetBooks', methods=['GET'])
+    def handle_GetBooks():
+        books = BookModel.query.order_by(BookModel.title).all()
+        results = [
+            {
+                'ISBN': book.ISBN,
+                'title': book.title,
+                'author': book.author
+            } for book in books]
+        return {"Books": results}
--- bookish/models/example.py
+++ bookish/models/example.py
@@ -1,25 +1,93 @@
 from bookish.app import db
 
 
-class Example(db.Model):
+class BookModel(db.Model):
     # This sets the name of the table in the database
-    __tablename__ = 'Test'
+    __tablename__ = 'BookModel'
 
     # Here we outline what columns we want in our database
-    id = db.Column(db.Integer, primary_key=True)
-    data1 = db.Column(db.String())
-    data2 = db.Column(db.String())
+    ISBN = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    author = db.Column(db.String())
 
-    def __init__(self, data1, data2):
-        self.data1 = data1
-        self.data2 = data2
+    def __init__(self, ISBN, title, author):
+        self.ISBN = ISBN
+        self.title = title
+        self.author = author
 
     def __repr__(self):
-        return '<id {}>'.format(self.id)
+        return '<id {}>'.format(self.ISBN)
 
     def serialize(self):
         return {
-            'id': self.id,
-            'data1': self.data1,
-            'data2': self.data2
+            'ISBN': self.ISBN,
+            'title': self.title,
+            'author': self.author
+        }
+
+class BookCopies(db.Model):
+    # This sets the name of the table in the database
+    __tablename__ = 'BookCopies'
+
+    # Here we outline what columns we want in our database
+    BookID = db.Column(db.Integer, primary_key=True)
+    ISBN = db.Column(db.Integer)
+
+    def __init__(self, ISBN):
+        self.ISBN = ISBN
+
+    def __repr__(self):
+        return '<id {}>'.format(self.BookID)
+
+    def serialize(self):
+        return {
+            'BookID': self.BookID,
+            'ISBN': self.ISBN
+        }
+
+class BorrowedBooks(db.Model):
+    # This sets the name of the table in the database
+    __tablename__ = 'BorrowedBooks'
+
+    # Here we outline what columns we want in our database
+    BookID = db.Column(db.Integer, primary_key=True)
+    UserID = db.Column(db.Integer)
+    DueDate = db.Column(db.Date)
+
+    def __init__(self, BookID, UserID, DueDate):
+        self.BookID = BookID
+        self.UserID = UserID
+        self.DueDate = DueDate
+
+    def __repr__(self):
+        return '<id {}>'.format(self.BookID)
+
+    def serialize(self):
+        return {
+            'BookID': self.BookID,
+            'UserID': self.UserID,
+            'DueDate': self.DueDate
+        }
+
+class Users(db.Model):
+    # This sets the name of the table in the database
+    __tablename__ = 'Users'
+
+    # Here we outline what columns we want in our database
+    UserID = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    password = db.Column(db.String)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<id {}>'.format(self.UserID)
+
+    def serialize(self):
+        return {
+            'UserID': self.UserID,
+            'username': self.username,
+            'password': self.password
         }
